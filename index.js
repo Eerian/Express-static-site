f180237da4b3019857008833877a82566ba763d7
Erion Pali <eerian00@gmail.com>
2019-11-04 18:44:26 -0500

    Initial files commited
    Basic Express app running to test
    Images and data.json files created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('App is working');
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000...");
+});
